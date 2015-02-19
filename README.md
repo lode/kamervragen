34c3f16cab8ca8fd1c4dc3f7c24c4f914457bf6e
Lode Claassen <lode@alsvanzelf.nl>
2015-02-19 09:27:06 +0100

    adds some styling and future direction

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# kamervragen
-Kamervragen van burgers
+# Kamervragen van burgers
+
+Asking the Dutch government about pressing issues.
+
+
+## To Do
+
+- Something interesting to read on the static pages
+- Sharing a statement
+- Adding questions to a statement
+- Ranking questions on a statement
+- Adding statements
+- Nicer styling for select boxes
+
+
+## Contributing
+
+Pull Requests or issues are welcome!
+
+
+## Licence
+
+MIT
--- public_html/index.php
+++ public_html/index.php
@@ -2,11 +2,51 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<title>Kamervragen over de betrokkenheid van Nederland bij wereldvrede</title>
+	
+	<link rel="stylesheet" type="text/css" href="/frontend/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/frontend/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/frontend/css/generic.css">
+	<link rel="stylesheet" type="text/css" href="/frontend/css/statement.css">
 </head>
 <body>
-	<h1>Kamervragen over de betrokkenheid van Nederland bij wereldvrede</h1>
-	<ol>
+<div class="container">
+	<nav class="navbar navbar-default" id="top-navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navigation-content">
+					<span class="sr-only">Navigatie</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">
+					<i class="fa fa-users"></i>
+					Kamervragen van burgers
+				</a>
+			</div>
+			
+			<div class="collapse navbar-collapse" id="top-navigation-content">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/bijdragen">Bijdragen</a></li>
+					<li><a href="/over">Over</a></li>
+					<li><a href="/contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<p class="question-header-prefix">
+		<i class="fa fa-question-circle"></i>
+		kamervragen over &hellip;
+	</p>
+	<h1 class="question-header">
+		de betrokkenheid van Nederland bij wereldvrede
+	</h1>
+	
+	<ol class="questions">
 		<li>Bent u bekend met de recente aanslagen in <select data-group="place-of-couter-attack"><option>Kopenhagen</option><option>Parijs</option><option>&hellip;</option></select>?</li>
 		<hr>
 		<li>Bent u zich ervan bewust dat Nederland goedkeuring heeft gegeven voor het aanvallen van doelen in <select data-group="place-of-attack"><option>Syrië</option><option>Irak</option><option>&hellip;</option></select>?</li>
@@ -57,8 +97,11 @@
 		</li>
 		<li>Bent u van mening dat het huidige beleid ten aanzien van instemming met aanvallen moet worden gewijzigd?</li>
 	</ol>
-	
-	<script type="text/javascript" src="/frontend/js/jquery-2.1.3.min.js"></script>
-	<script type="text/javascript" src="/frontend/js/statement.js"></script>
+</div>
+
+<script type="text/javascript" src="/frontend/js/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="/frontend/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/frontend/js/statement.js"></script>
+
 </body>
 </html>
